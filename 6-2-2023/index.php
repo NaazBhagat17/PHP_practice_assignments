6a68ee451dfaf5dc49333e03c5d522c79a8223d3
Naaz Bhagat <123932153+NaazBhagat17@users.noreply.github.com>
2023-02-08 13:07:25 +0530

    Add files via upload

---
+++ 6-2-2023/index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <h3>Write a program to print two variables in single echo
+Conditions:
+First variable have text "Good Morning."
+Second variable have text "Have a nice day!"
+Your output should be "Good morning. Have a nice day!"
+You are allowed to use only one echo statement in this program.</h3>
+    </body>
+</html>
+<?php
+$a = "Good Morning. ";
+$b = "Have a nice day!";
+echo $a . $b;
+?>
